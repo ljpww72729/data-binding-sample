bcf6e5a56896c44ba3917e8a0991648c9ff8ca0b
lipeng <lipeng@linkedme.cc>
2017-04-13 12:08:42 +0800

    Test get user info.

--- app/src/main/java/cc/lkme/databindingsample/MainActivity.java
+++ app/src/main/java/cc/lkme/databindingsample/MainActivity.java
@@ -3,8 +3,16 @@ package cc.lkme.databindingsample;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import cc.lkme.databindingsample.databinding.ActivityMainBinding;
 import cc.lkme.databindingsample.model.UserInfo;
@@ -17,10 +25,32 @@ public class MainActivity extends AppCompatActivity {
         //默认
 //        setContentView(R.layout.activity_main);
         //data binding
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.helloBinding.setText("Hello data binding, you are so awesome!");
 //        该种方式比较原始,不会用到反射,具体查看一下API
 //      binding.setVariable(BR.defaultAddress, "无地址");
+        File file = new File(Environment.getExternalStorageDirectory(), "test.txt");
+//        File dir = new File(Environment.getExternalStorageDirectory(), "abc");
+//        dir.mkdirs();
+//        file.exists();
+//        try {
+//            file.createNewFile();
+//            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(file));
+//            outputStreamWriter.write("nihao");
+//            outputStreamWriter.flush();
+//            outputStreamWriter.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String abc = bufferedReader.readLine();
+            Toast.makeText(MainActivity.this, abc, Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         binding.setDefaultAddress("地址不存在!");
 
 //        //Observable Objects
@@ -38,6 +68,12 @@ public class MainActivity extends AppCompatActivity {
         userInfo.address.set("北京市");
 
         binding.setUser(userInfo);
+        binding.getUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, binding.getUser().name.get(), Toast.LENGTH_SHORT).show();
+            }
+        });
         //设置Handler
         binding.setHandlers(new MainHandler(this));
         binding.recycleView.setOnClickListener(new View.OnClickListener() {
